976315698701101a7c744b60c71b767797d9d515
Salvador Medina <salvadom@andrew.cmu.edu>
2015-12-02 16:27:35 -0500

    UPDATE BoW workspace with BoW and ADD transform function

--- DataAnalysis/extract_all_dsift.m
+++ DataAnalysis/extract_all_dsift.m
@@ -1,8 +1,8 @@
-function descriptors=extract_all_dsift(data,win_size)
-    [~, descriptors]=extract_dsift(data,1,win_size);
-    [num_images,~]=size(data);
+function descriptors=extract_all_dsift(batch_data,win_size)
+    [~, descriptors]=extract_dsift(batch_data,1,win_size);
+    [num_images,~]=size(batch_data);
     for i=2:num_images
-        [~, d_i]=extract_dsift(data,i,win_size);
+        [~, d_i]=extract_dsift(batch_data,i,win_size);
         descriptors=[descriptors d_i];
     end
 end
