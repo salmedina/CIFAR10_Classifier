938f2e77796c0430ecab5c717f8fb20e437066dd
Salvador Medina <salvadom@andrew.cmu.edu>
2015-12-02 18:25:59 -0500

    UPDATE Submission KNN, removed HOG feature vector transformation

--- Submission/12.1 47.9+11.3(GNB)/knn.m
+++ Submission/12.1 47.9+11.3(GNB)/knn.m
@@ -1,4 +1,4 @@
-function [ new_feat ] = knn( origin_feat, centroids )
+function [ indices ] = knn( origin_feat, centroids )
 % turn the test set into bow features
 
     N = size(origin_feat, 1); 
@@ -22,34 +22,6 @@ function [ new_feat ] = knn( origin_feat, centroids )
     end
     
     % Get belonging cluster
-    [z,cur_cluster]=min(Distance,[],2); %z distance, g_cur belonging cluster
-   
-    % get the word vectore
-    temp_vec = cell(N, 1);
-    for i = 1:N
-        temp = cur_cluster((16*i - 15):(16*i), 1);
-        temp = temp';
-        temp_vec{i} = temp;
-    end
-    temp_vec = cell2mat(temp_vec); 
-    
-    new_feat = zeros(N, K);
-    for i = 1:N
-        A = temp_vec(i, :);
-        % count the occurance
-        A2 = unique(A);
-        count = histc(A, A2);
-        
-        % set the new feature vector
-        for k = 1:size(A2, 2)
-            new_feat(i, A2(k)) = count(k);
-        end
-    end
-    
-    % normaliaze the feature vector
-    for i = 1:N
-        new_feat(i,:) = new_feat(i,:) ./ sqrt(sum(new_feat(i,:).^2));
-    end
-    %new_feat = normr(new_feat);
+    [distances,indices]=min(Distance,[],2); %z distance, g_cur belonging cluster
 end
 
