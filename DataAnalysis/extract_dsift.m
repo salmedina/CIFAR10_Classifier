ca78a5a347714267e14955378dc5c25935e54332
Salvador Medina <salvadom@andrew.cmu.edu>
2015-12-03 02:32:26 -0500

    UPDATE data analysis tools: all_sift, all_hog

--- DataAnalysis/extract_dsift.m
+++ DataAnalysis/extract_dsift.m
@@ -1,3 +1,3 @@
-function [f,d]=extract_dsift(batch_data, index_num, winSize)
-    [f,d]=vl_dsift(im2single(rgb2gray(reshape(batch_data(index_num,:),32,32,3))),'size',winSize);
+function [f,d]=extract_dsift(batch_data, index_num, win_size)
+    [f,d]=vl_dsift(im2single(rgb2gray(reshape(batch_data(index_num,:),32,32,3))),'size',win_size);
 end
