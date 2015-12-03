592b0bee15a198d979932920610d2e70d8bcf3d9
Salvador Medina <salvadom@andrew.cmu.edu>
2015-12-03 02:30:59 -0500

    UPDATE changes to logistic regression classifier

---
+++ Logistic/multinomial_logistic.m
@@ -0,0 +1,8 @@
+function [prob, Y]=multinomial_logistic(X, multi_theta)
+    simgoid = @(z) 1.0/(1.0+exp(-z));
+    %add Intercept
+    [m,n]=size(X);
+    X = [ones(m,1), X];
+    [prob, Y] = max(sigmoid(X*multi_theta'),[],2);
+    Y=Y-1; %Trained for class labels [1...10]
+end
