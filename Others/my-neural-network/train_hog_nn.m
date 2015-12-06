a063ef9728ec099bddcf69d9e2032c9f3ca43516
Salvador Medina <salvadom@andrew.cmu.edu>
2015-12-06 12:25:27 -0500

    UPDATE neural network training for dual layer

--- Others/my-neural-network/train_hog_nn.m
+++ Others/my-neural-network/train_hog_nn.m
@@ -3,5 +3,5 @@ display('Environment loaded');
 display('Begin NN training');
 [hog_model hog_cc_train hog_output_train] = train_mlp(hog_descriptors, Y_10, [100 100], 1000, 0.1, 0.5);
 display('Finished training');
-save('trained_hog_nn.m');
+save('trained_hog_nn_100_100.m');
 display('Done');
