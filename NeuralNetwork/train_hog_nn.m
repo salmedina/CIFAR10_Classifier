4109365bcf165ea99c774509123cdec4a68f00d9
Salvador Medina <salvadom@andrew.cmu.edu>
2015-12-06 12:35:50 -0500

    ADD new network training script

--- NeuralNetwork/train_hog_nn.m
+++ NeuralNetwork/train_hog_nn.m
@@ -1,4 +1,4 @@
-load('NN_train_setup.mat');
+load('NN_train_setup.m');
 display('Environment loaded');
 display('Begin NN training');
 [hog_model hog_cc_train hog_output_train] = train_mlp(hog_descriptors, Y_10, [100 100], 1000, 0.1, 0.5);
