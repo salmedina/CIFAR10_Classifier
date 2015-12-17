5f8cc77e5bba81a524fc71a57b92cae530f10877
Salvador Medina <salvadom@andrew.cmu.edu>
2015-12-17 00:37:20 -0500

    UPDATE training setting for single hidden layer 50,100,200

--- NeuralNetwork/train_hog_nn.m
+++ NeuralNetwork/train_hog_nn.m
@@ -1,7 +1,18 @@
 load('NN_train_setup.m');
 display('Environment loaded');
-display('Begin NN training');
-[hog_model hog_cc_train hog_output_train] = train_mlp(hog_descriptors, Y_10, [100 100], 1000, 0.1, 0.5);
+display('Begin NN [50]');
+[hog_model hog_cc_train hog_output_train] = train_mlp(hog_descriptors, Y_10, [50], 700, 0.1, 0.5);
 display('Finished training');
-save('trained_hog_nn.mat');
+save('trained_hog_nn_50.mat');
+
+display('Begin NN [100]');
+[hog_model hog_cc_train hog_output_train] = train_mlp(hog_descriptors, Y_10, [100], 700, 0.1, 0.5);
+display('Finished training');
+save('trained_hog_nn_100.mat');
+
+display('Begin NN [200]');
+[hog_model hog_cc_train hog_output_train] = train_mlp(hog_descriptors, Y_10, [200], 700, 0.1, 0.5);
+display('Finished training');
+save('trained_hog_nn_200.mat');
+
 display('Done');
